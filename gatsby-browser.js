3f69a7c7dce68a41b3e8712f044c5d01d06a0ff1
ChangMarkusYu <marcus.yu.56@gmail.com>
2021-12-31 20:50:04 -0500

    refactored root element wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,27 +1,17 @@
 import React from "react"
-import { useState } from "react";
-import { UIFrame } from "./src/components/UIFrame";
 import { CssBaseline } from "@mui/material";
-import { DarkTheme, LightTheme } from "./src/components/Theme";
-import { ThemeProvider } from "@emotion/react";
-import { createTheme } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import { ThemeWrapper } from "./src/components/Theme";
 
 // Wraps every page in a component
 export const wrapRootElement = ({ element }) => {
-    const [dark, setDark] = useState(true);
-    const changeTheme = () => {
-        setDark(!dark);
-    }
-
     return (
         <CssBaseline>
-            <ThemeProvider theme={dark ? createTheme(DarkTheme) : createTheme(LightTheme)}>
-            <UIFrame changeTheme={changeTheme} dark={dark}/>
+            <ThemeWrapper>
                 <Paper elevation={9} square sx={{pt:15, pl:6, pb:5,flexGrow:1, minHeight:"100vh"}}>
                     {element}
                 </Paper>
-            </ThemeProvider>
+            </ThemeWrapper>
         </CssBaseline>
     );
 }
--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,4 +1,10 @@
-export const DarkTheme = {
+import React from "react"
+import { useState } from "react";
+import { UIFrame } from "./UIFrame";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from '@mui/material/styles';
+
+const DarkTheme = {
     palette: {
         mode: 'dark',
     },
@@ -9,7 +15,7 @@ export const DarkTheme = {
     },
 }
 
-export const LightTheme = {
+const LightTheme = {
     palette: {
         mode: 'light',
     },
@@ -19,4 +25,18 @@ export const LightTheme = {
         },
     },
 };
+
+export const ThemeWrapper = (props) => {
+    const [dark, setDark] = useState(true);
+    const changeTheme = () => {
+        setDark(!dark);
+    }
+
+    return (
+        <ThemeProvider theme={dark ? createTheme(DarkTheme) : createTheme(LightTheme)}>
+            <UIFrame changeTheme={changeTheme} dark={dark}/>
+            {props.children}
+        </ThemeProvider>
+    );
+}
   
