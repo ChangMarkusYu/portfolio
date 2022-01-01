6a05b6422b5ffa2e047e2b3af28d9828547c78d9
ChangMarkusYu <marcus.yu.56@gmail.com>
2021-12-31 19:16:15 -0500

    wrapRootElement config

---
+++ gatsby-browser.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { useState } from "react";
+import { UIFrame } from "./components/UIFrame";
+import { CssBaseline } from "@mui/material";
+import { DarkTheme, LightTheme } from "./components/Theme";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+// Wraps every page in a component
+export function wrapRootElement({ element }) {
+    const [dark, setDark] = useState(true);
+    const changeTheme = () => {
+        setDark(!dark);
+    }
+
+    return (
+        <CssBaseline>
+            <ThemeProvider theme={dark ? createTheme(DarkTheme) : createTheme(LightTheme)}>
+            <UIFrame changeTheme={changeTheme} dark={dark}/>
+                <Paper elevation={9} square sx={{pt:15, pl:6, pb:5,flexGrow:1, minHeight:"100vh"}}>
+                    {element}
+                </Paper>
+            </ThemeProvider>
+        </CssBaseline>
+    );
+}
