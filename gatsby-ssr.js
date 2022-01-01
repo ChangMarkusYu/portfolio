b71160d3574cd7124e1ddb5218cc5b147723fe05
ChangMarkusYu <marcus.yu.56@gmail.com>
2021-12-31 19:43:51 -0500

    removed root styling

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,7 +6,6 @@ import { DarkTheme, LightTheme } from "./src/components/Theme";
 import { ThemeProvider } from "@emotion/react";
 import { createTheme } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
-import { root } from './src/components/layout.module.css'
 
 // Wraps every page in a component
 export function wrapRootElement({ element }) {
@@ -16,7 +15,6 @@ export function wrapRootElement({ element }) {
     }
 
     return (
-        <div className={root}>
         <CssBaseline>
             <ThemeProvider theme={dark ? createTheme(DarkTheme) : createTheme(LightTheme)}>
             <UIFrame changeTheme={changeTheme} dark={dark}/>
@@ -25,6 +23,5 @@ export function wrapRootElement({ element }) {
                 </Paper>
             </ThemeProvider>
         </CssBaseline>
-        </div>
     );
 }
