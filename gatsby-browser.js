acca2d5d1c7ab2287ca52f6e0f7dd2b57d0a27ea
ChangMarkusYu <marcus.yu.56@gmail.com>
2022-01-03 15:03:27 -0500

    patched prismjs layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { RootWrapper } from "./src/components/RootWrapper";
+require("prismjs/themes/prism-okaidia.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("./src/assets/prism-layout.css")
 
 // Wraps every page in a component
 export const wrapRootElement = ({ element }) => {
