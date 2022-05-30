f1c84d771c5ded12da8474e3a59441a8a57aaa41
Chang Yu <marcus.yu.56@gmail.com>
2022-05-30 13:26:39 -0700

    added gatsby-remark-autolink-headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,9 @@ module.exports = {
             {
                 plugins: [
                     {
+                        resolve: `gatsby-remark-autolink-headers`
+                    },
+                    {
                         resolve: `gatsby-remark-prismjs`,
                         options: 
                         {
